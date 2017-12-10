8aa7a8ae3b83e232a176aac8fbf7a7fbd1d70920
Roland Tecson <roland_tecson@hotmail.com>
2017-12-10 13:59:40 -0800

    Initial commit

--- w6d3-fetchedresultscontroller/DetailViewController.swift
+++ w6d3-fetchedresultscontroller/DetailViewController.swift
@@ -10,36 +10,35 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    // MARK: - IBOutlets
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.timestamp!.description
-            }
-        }
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        configureView()
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    var detailItem: Event? {
+    // MARK: - Public properties
+    var detailItem: Meal? {
         didSet {
             // Update the view.
             configureView()
         }
     }
-
-
 }
 
+// MARK: - UIViewController methods
+extension DetailViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+}
+
+// MARK: - Private methods
+extension DetailViewController {
+    
+    private func configureView() {
+        // Update the user interface for the detail item.
+        if let detailItem = detailItem, let label = detailDescriptionLabel, let mealName = detailItem.name {
+            label.text = "\(mealName) - \(detailItem.calories)"
+        }
+    }
+}
--- w6d3-fetchedresultscontroller/MasterViewController.swift
+++ w6d3-fetchedresultscontroller/MasterViewController.swift
@@ -11,17 +11,25 @@ import CoreData
 
 class MasterViewController: UITableViewController, NSFetchedResultsControllerDelegate {
 
+    // MARK: - Public properties
     var detailViewController: DetailViewController? = nil
     var managedObjectContext: NSManagedObjectContext? = nil
+    
+    // MARK: - IBActions
+    @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
+        insertNewObject(sender)
+    }
 
+    // MARK: - Private properties
+    var _fetchedResultsController: NSFetchedResultsController<Meal>? = nil  // Stored property below
+}
+
+// MARK: - UIViewController methods
+extension MasterViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        navigationItem.leftBarButtonItem = editButtonItem
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-        navigationItem.rightBarButtonItem = addButton
         if let split = splitViewController {
             let controllers = split.viewControllers
             detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
@@ -33,43 +41,21 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         super.viewWillAppear(animated)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @objc
-    func insertNewObject(_ sender: Any) {
-        let context = self.fetchedResultsController.managedObjectContext
-        let newEvent = Event(context: context)
-             
-        // If appropriate, configure the new managed object.
-        newEvent.timestamp = Date()
-
-        // Save the context.
-        do {
-            try context.save()
-        } catch {
-            // Replace this implementation with code to handle the error appropriately.
-            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            let nserror = error as NSError
-            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-        }
-    }
-
     // MARK: - Segues
-
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
-            if let indexPath = tableView.indexPathForSelectedRow {
-            let object = fetchedResultsController.object(at: indexPath)
-                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-                controller.detailItem = object
+            if let indexPath = tableView.indexPathForSelectedRow, let segueDestination = segue.destination as? UINavigationController, let controller = segueDestination.topViewController as? DetailViewController {
+                let meal = fetchedResultsController.object(at: indexPath)
+                controller.detailItem = meal
                 controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
                 controller.navigationItem.leftItemsSupplementBackButton = true
             }
         }
     }
+}
+
+// MARK: - UITableViewDatasource methods
+extension MasterViewController {
 
     // MARK: - Table View
 
@@ -84,10 +70,14 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let event = fetchedResultsController.object(at: indexPath)
-        configureCell(cell, withEvent: event)
+        let meal = fetchedResultsController.object(at: indexPath)
+        configureCell(cell, withMeal: meal)
         return cell
     }
+}
+
+// MARK: - UITableViewDelegate methods
+extension MasterViewController {
 
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
@@ -110,24 +100,28 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         }
     }
 
-    func configureCell(_ cell: UITableViewCell, withEvent event: Event) {
-        cell.textLabel!.text = event.timestamp!.description
+    func configureCell(_ cell: UITableViewCell, withMeal meal: Meal) {
+        if let mealName = meal.name {
+            cell.textLabel!.text = "\(mealName) - \(meal.calories)"
+        }
     }
+}
 
-    // MARK: - Fetched results controller
+// MARK: - NSFetchedResultsControllerDelegate methods
+extension MasterViewController {
 
-    var fetchedResultsController: NSFetchedResultsController<Event> {
+    var fetchedResultsController: NSFetchedResultsController<Meal> {
         if _fetchedResultsController != nil {
             return _fetchedResultsController!
         }
         
-        let fetchRequest: NSFetchRequest<Event> = Event.fetchRequest()
+        let fetchRequest: NSFetchRequest<Meal> = Meal.fetchRequest()
         
         // Set the batch size to a suitable number.
         fetchRequest.fetchBatchSize = 20
         
         // Edit the sort key as appropriate.
-        let sortDescriptor = NSSortDescriptor(key: "timestamp", ascending: false)
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: false)
         
         fetchRequest.sortDescriptors = [sortDescriptor]
         
@@ -148,7 +142,6 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
         
         return _fetchedResultsController!
     }    
-    var _fetchedResultsController: NSFetchedResultsController<Event>? = nil
 
     func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.beginUpdates()
@@ -172,9 +165,9 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
             case .delete:
                 tableView.deleteRows(at: [indexPath!], with: .fade)
             case .update:
-                configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Event)
+                configureCell(tableView.cellForRow(at: indexPath!)!, withMeal: anObject as! Meal)
             case .move:
-                configureCell(tableView.cellForRow(at: indexPath!)!, withEvent: anObject as! Event)
+                configureCell(tableView.cellForRow(at: indexPath!)!, withMeal: anObject as! Meal)
                 tableView.moveRow(at: indexPath!, to: newIndexPath!)
         }
     }
@@ -182,15 +175,53 @@ class MasterViewController: UITableViewController, NSFetchedResultsControllerDel
     func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.endUpdates()
     }
-
-    /*
-     // Implementing the above methods to update the table view in response to individual changes may have performance implications if a large number of changes are made simultaneously. If this proves to be an issue, you can instead just implement controllerDidChangeContent: which notifies the delegate that all section and object changes have been processed.
-     
-     func controllerDidChangeContent(controller: NSFetchedResultsController) {
-         // In the simplest, most efficient, case, reload the table view.
-         tableView.reloadData()
-     }
-     */
-
 }
 
+// MARK: - Private methods
+extension MasterViewController {
+    
+    @objc
+    private func insertNewObject(_ sender: Any) {
+        let context = fetchedResultsController.managedObjectContext
+        
+        let alert = UIAlertController(title: "Add a Meal", message: nil, preferredStyle: .alert)
+        
+        var nameTextField: UITextField!
+        var caloriesTextField: UITextField!
+        
+        alert.addTextField { (textField: UITextField) in
+            nameTextField = textField
+        }
+        alert.addTextField { (textField: UITextField) in
+            caloriesTextField = textField
+        }
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction) in
+            print("name is \(String(describing: nameTextField.text))")
+            print("calories is \(String(describing: caloriesTextField.text))")
+            
+            // Create a new managed object using the user inputted values
+            let newMeal = Meal(context: context)
+            newMeal.name = nameTextField.text
+            if let caloriesText = caloriesTextField.text, let calories = Int64(caloriesText) {
+                newMeal.calories = calories
+            }
+
+            // Save the context.
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action: UIAlertAction) in
+            // Do nothing
+        }))
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
