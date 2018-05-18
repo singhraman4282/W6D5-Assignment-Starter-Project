f9549d77c00550a20e4f55bae7ad6506621c47d9
Raman Singh <rami88@ymail.com>
2018-05-18 13:32:22 -0700

    Just started

---
+++ w6d5-ui-performance-testingUITests/w6d5_ui_performance_testingUITests.swift
@@ -0,0 +1,79 @@
+//
+//  w6d5_ui_performance_testingUITests.swift
+//  w6d5-ui-performance-testingUITests
+//
+//  Created by Raman Singh on 2018-05-18.
+//  Copyright © 2018 Roland. All rights reserved.
+//
+
+import XCTest
+
+class w6d5_ui_performance_testingUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testUI() {
+        
+//        let app = XCUIApplication()
+//        app.navigationBars["Master"].buttons["Add"].tap()
+//
+//        let addAMealAlert = app.alerts["Add a Meal"]
+//        let collectionViewsQuery = addAMealAlert.collectionViews
+//        collectionViewsQuery.children(matching: .other).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .textField).element.typeText("Burger")
+//
+//        let textField = collectionViewsQuery.children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .textField).element
+//        textField.tap()
+//        textField.typeText("300")
+//        addAMealAlert.buttons["Ok"].tap()
+//
+        
+        
+        
+        let app2 = XCUIApplication()
+        app2.navigationBars["Master"].buttons["Add"].tap()
+        
+        let addAMealAlert = app2.alerts["Add a Meal"]
+        let collectionViewsQuery = addAMealAlert.collectionViews
+        let textField = collectionViewsQuery.children(matching: .other).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .textField).element
+        textField.tap()
+        textField.tap()
+        
+        let rKey = app2/*@START_MENU_TOKEN@*/.keys["r"]/*[[".keyboards.keys[\"r\"]",".keys[\"r\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        rKey.tap()
+        rKey.tap()
+        
+        let tKey = app2/*@START_MENU_TOKEN@*/.keys["t"]/*[[".keyboards.keys[\"t\"]",".keys[\"t\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        tKey.tap()
+        tKey.tap()
+        app2/*@START_MENU_TOKEN@*/.buttons["Return"]/*[[".keyboards",".buttons[\"return\"]",".buttons[\"Return\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        collectionViewsQuery.children(matching: .other).element(boundBy: 1).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .textField).element.tap()
+        
+        let fKey = app2/*@START_MENU_TOKEN@*/.keys["f"]/*[[".keyboards.keys[\"f\"]",".keys[\"f\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        fKey.tap()
+        fKey.tap()
+        addAMealAlert.buttons["Ok"].tap()
+       
+        
+
+        
+        
+    }//testUI
+    
+    
+}
